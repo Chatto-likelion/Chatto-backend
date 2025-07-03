2990b77bd334aeb07c233853ae9603b98e100c7f
jych <jych1109@gmail.com>
2025-07-04 00:13:22 +0900

    feat: play_analysis 구현

--- play/urls.py
+++ play/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import ChatView, ChatListView, ChatDetailView, ChatAnalyzeView
+from .views import AnalysisListView, AnalysisDetailView
 
 app_name = 'play'
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path('chats/<int:user_id>/', ChatListView.as_view()),
     path('chats/<int:chat_id>/delete/', ChatDetailView.as_view()),
     path('chats/<int:chat_id>/analyze/', ChatAnalyzeView.as_view()),
+
+    path('analysis/<int:user_id>/', AnalysisListView.as_view()),
+    path('analysis/<int:result_id>/detail/', AnalysisDetailView.as_view()),
 ]
