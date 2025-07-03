2a7aa1706fc762b0d0545c36e7afa4f200e59d65
hyukjun <todd4@naver.com>
2025-07-04 01:09:39 +0900

    feat: business results 완료

--- business/urls.py
+++ business/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BusChatUploadView, BusChatListView, BusChatDetailView, BusChatAnalyzeView
+from .views import BusChatUploadView, BusChatListView, BusChatDetailView, BusChatAnalyzeView, BusResultListView, BusResultDetailView
 
 app_name = 'business'
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path('chat/<int:user_id>/', BusChatListView.as_view()),  # Assuming you want to list chats for a specific user
     path('chat/<int:chat_id>/delete', BusChatDetailView.as_view()),  # Assuming you want to delete a specific chat by ID
     path('chat/<int:chat_id>/analyze/', BusChatAnalyzeView.as_view()),  # Assuming you want to analyze a specific chat by ID
+    
+    path('analysis/<int:user_id>/', BusResultListView.as_view()),  # Assuming you want to list analysis results for a specific user
+    path('analysis/<int:result_id>/detail/', BusResultDetailView.as_view()),  # Assuming you want to get details of a specific analysis result by ID')
 ]
